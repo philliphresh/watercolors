76e16a104193422305f06955e24aae20891cc303
Phillip Sanderell <Phillip@Phillips-MacBook-Pro.local>
2022-12-18 20:29:40 -0500

    adding code to actually add songs to playlist

--- Create Spotify Playlist.R
+++ Create Spotify Playlist.R
@@ -1,41 +1,42 @@
 library(tidyverse)
 library(spotifyr)
 
-# set credentials
+# Set credentials
 # I put my actual client id and client secret in the console
 # Sys.setenv(SPOTIFY_CLIENT_ID = 'xxxxxxxxxxxxxxxxxxxxx')
 # Sys.setenv(SPOTIFY_CLIENT_SECRET = 'xxxxxxxxxxxxxxxxxxxxx')
 
 access_token <- get_spotify_access_token()
 
-# Search for a song
-test_search_results <-
-  search_spotify(q = "track:Morning Breeze artist:Jazz Holdouts",
-                 type = "track",
-                 limit = 1)
+# Import track data
+tracks <- 
+  read_csv(list.files(pattern = "csv$") %>% tail(1))
 
-test_search_results$uri
 
 # Get URIs of songs
-# songs_clean <- read.csv()
-
 search_input <- 
-  songs_clean %>% 
+  tracks %>% 
   mutate(artist = str_replace_all(artist, "/", ", ")) %>% 
   mutate(query = paste0("track:", song, " ",
                         "artist:", artist))
 
-test_search_results <- 
+#   This line will take some time to run since it is making as 
+#   many requests as the number of tracks.
+#   Wrapping the results in a list helps the code run even
+#   when the query returns zero search results.
+search_results <- 
   search_input %>% 
-  head(3) %>% 
   rowwise() %>% 
-  mutate(search_results = search_spotify(query,
+  mutate(search_results = list(search_spotify(query,
                                          type = "track",
-                                         limit = 1))
+                                         limit = 1)))
 
-test_search_results$search_results$uri
+# When the search_results column is unnested it will not
+# create any rows for empty resutls thus filtering out 
+# tracks that didn't return a match.
 
-# Start by creating an empty playlist
+
+# Now create an empty playlist
 # (I had a lot of issues getting the authentication to work here
 # I ended up needing to set my Redirect URI to http://localhost:1410/
 # in the Spotify developer dashboard.)
@@ -47,4 +48,53 @@ create_playlist(
     get_spotify_authorization_code(scope = "playlist-modify-public")
 )
 
+# Figure out playlist id
+my_playlists <- get_my_playlists()
+
+playlist_id <- 
+  my_playlists %>% 
+  filter(name == "WatercoloRs") %>% 
+  pull(id)
+
+# See what tracks are already on the playlist
+#   The first time the playlist is made this should be empty 
+#   but when I add more songs to the playlist it will not be.
+current_playlist_tracks <- get_playlist_tracks(playlist_id = playlist_id)
+
+# Remove tracks that are already on the playlist from the list to add
+filtered_search_results <- 
+  search_results %>% 
+  unnest(search_results) %>% 
+  filter(!(uri %in% current_playlist_tracks$track.uri))
+
 # Add tracks to playlist
+#   since a maximum of 100 tracks can be added in one request, 
+#   I am looping through tracks rather than adding all tracks in 
+#   one call of the function.
+
+# purrr::walk(search_results$search_results$uri, 
+#             .f = function(x) add_tracks_to_playlist(playlist_id = playlist_id,
+#                                                     uris = x))
+
+# The above is not working, my guess is that it needs to add more than one track at a time
+
+# The code below loops through 100 indices of tracks at a time
+for (i in seq_along(1:(nrow(filtered_search_results) %/% 100 + 1))) {
+  uris <- 
+    filtered_search_results$uri[seq(1 + 100 * (i - 1), 100 * (i), 1)]
+  
+  uris <- uris[!is.na(uris)]
+  
+  add_tracks_to_playlist(playlist_id = playlist_id,
+                         uris = uris)
+}
+
+
+# Now I can check for duplicate songs and remove them if necessary
+current_playlist_tracks <- get_playlist_tracks(playlist_id = playlist_id)
+
+current_playlist_tracks %>% 
+  tibble() %>% 
+  count(track.uri, sort = TRUE)
+
+# no duplicates for now so we're good to go!
